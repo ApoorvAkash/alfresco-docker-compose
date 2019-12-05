616efcbe3ec3ef4f6e09af415e994230825f6b8b
apoorvakash <apoorvakash@outlook.com>
2019-12-05 20:34:23 +0530

    Api Server fix

--- api-server/api-server.rb
+++ api-server/api-server.rb
@@ -9,21 +9,24 @@ post '/payload' do
     if package['package_type'] == 'maven'
       package_files = package['package_version']['package_files']
       package_files.each { |asset| downloadUrl =  asset['download_url']
+      if tempFile.original_filename.end_with? "jar"
         begin
           tempFile = Down.download(downloadUrl);
-          if tempFile.original_filename.end_with? "jar"
+            originalFilename = tempFile.original_filename
+            puts originalFilename;
             deleteFile = tempFile.original_filename[0..5].concat('*');
-            system("find ./target/maven/ -type f -name #{deleteFile} -delete");
+            system("pwd")
+            system("find ./api-server/target/maven/ -type f -name #{deleteFile} -delete");
             system("sudo docker container stop aps");
             system("sudo docker exec aps find /usr/share/tomcat/webapps/activiti-app/WEB-INF/lib -type f -name #{deleteFile} -delete");
-            FileUtils.mv(tempFile.path, "./target/maven/#{tempFile.original_filename}");
+            FileUtils.mv(tempFile.path, "./api-server/target/maven/#{originalFilename}");
             system("sudo docker cp /home/ubuntu/alfresco-docker-compose/api-server/target/maven/#{tempFile.original_filename} aps:/usr/share/tomcat/webapps/activiti-app/WEB-INF/lib");
             system("sudo docker container start aps");
-          end
         rescue => exception
           puts "Exception Message: #{ exception.message }"
           puts "Exception Backtrace: #{ exception.backtrace }"
         end
+      end
       }
     if package['package_type'] == 'npm'
       puts 'Write Code here for npm packaging'
